33a40d0a17020d10d7d9cf410c5e19fb440e45e5
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-12 19:05:31 +0300

    Создание Repository.kt

---
+++ app/src/main/java/com/example/informationbin/data/Repository.kt
@@ -0,0 +1,32 @@
+package com.example.informationbin.data
+
+import android.app.Application
+import androidx.room.Room
+import com.example.informationbin.App
+import com.example.informationbin.data.emptities.BinDetail
+import com.example.informationbin.data.emptities.HistoryElement
+import com.example.informationbin.data.networkService.BINService
+import com.example.informationbin.data.networkService.RetrofitClient
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+
+object Repository {
+    private val localDb = App.roomDatabase.historyDao()
+    private val networkDb = RetrofitClient.getRetrofitService().create(BINService::class.java)
+
+    fun getBinInformation(bin: String): Call<BinDetail> {
+        return networkDb.getBinInformation(bin)
+    }
+
+    suspend fun getHistory(): List<HistoryElement> {
+        return localDb.getAll()
+    }
+
+    suspend fun addElementInHistory(historyElement: HistoryElement) {
+        return localDb.insert()
+    }
+
+    suspend fun deleteHistory(historyElement: HistoryElement) {
+        return localDb.delete(historyElement)
+    }
+}
