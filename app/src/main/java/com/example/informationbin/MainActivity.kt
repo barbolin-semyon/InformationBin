5611d68e00b7bdf9e9ca50140f286ee06f96d353
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-13 17:59:15 +0300

    Подключение нижнего меню навигации и navGraph в MainActivity.kt

--- app/src/main/java/com/example/informationbin/MainActivity.kt
+++ app/src/main/java/com/example/informationbin/MainActivity.kt
@@ -3,21 +3,33 @@ package com.example.informationbin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.informationbin.ui.navigation.AppBottomNavigation
+import com.example.informationbin.ui.navigation.NavGraph
 import com.example.informationbin.ui.theme.InformationBinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InformationBinTheme {
+            val navController = rememberNavController()
 
+            InformationBinTheme {
+                Scaffold(bottomBar = { AppBottomNavigation(navHostController = navController) }) {
+                    Box(Modifier.padding(it)) {
+                        NavGraph(navController = navController)
+                    }
+                }
             }
         }
     }
