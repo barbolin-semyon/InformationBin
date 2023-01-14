993054f32fc8d302dce2ba7e5c433b3baa037a06
4118BarbolinS.P <skat.apps@gmail.com>
2023-01-14 15:41:31 +0300

    Исправление get запроса

--- app/src/main/java/com/example/informationbin/data/networkService/BINService.kt
+++ app/src/main/java/com/example/informationbin/data/networkService/BINService.kt
@@ -3,9 +3,10 @@ package com.example.informationbin.data.networkService
 import com.example.informationbin.data.emptities.BinDetail
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BINService {
-    @GET("/")
-    fun getBinInformation(@Query("name") bin: String): Call<BinDetail>
+    @GET("/{name}")
+    fun getBinInformation(@Path("name") bin: String): Call<BinDetail>
 }
